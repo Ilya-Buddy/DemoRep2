a32f449725819974df6aaa0f0b41e4049646a491
buddy <budman@list.ru>
2021-12-20 23:24:14 +0500

    add

---
+++ Task002/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = 22;
+int numberB = 33;
+
+if (numberA > numberB)
+{
+    Console.Write("Большее число"); Console.Write (numberA);
+}
+else 
+{
+    Console.Write("Большее число"); Console.Write(numberB);
+}
