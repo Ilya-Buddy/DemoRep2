69af3ce4a58afd28c5d0fe230e01698dd8d08c1a
buddy <budman@list.ru>
2022-01-17 14:11:33 +0500

    1

--- Test_Probnik/Program.cs
+++ Test_Probnik/Program.cs
@@ -3,14 +3,10 @@ int count = rnd.Next(20, 21);
 string str = string.Empty;
 int r;
 char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|".ToCharArray();
-for (int i = 0; i < count; i++) {
+for (int i = 0; i < count; i++) 
+{
     r = rnd.Next(chars.Length);
     str += chars[r];
 }
 
 Console.WriteLine(str);
-
-
-
-
-
